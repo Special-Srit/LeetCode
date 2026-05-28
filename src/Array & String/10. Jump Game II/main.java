d81491a0caab0fbf6761ffeccdb57280ad5331fb
srit <coder.srit@gmail.com>
2026-05-28 22:57:44 +0900

    Solved `45. Jump Game II`

---
+++ src/Array & String/10. Jump Game II/main.java
@@ -0,0 +1,29 @@
+class JumpGameII {
+    static class Solution {
+        public int jump(int[] nums) {
+            int jumps = 0;
+            int currentEnd = 0;
+            int farthest = 0;
+
+            for (int i = 0; i < nums.length - 1; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+
+                if (i == currentEnd) {
+                    jumps++;
+                    currentEnd = farthest;
+
+                    if (currentEnd >= nums.length - 1) {
+                        break;
+                    }
+                }
+            }
+
+            return jumps;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.jump(new int[]{2,3,1,1,4}));
+    }
+}
