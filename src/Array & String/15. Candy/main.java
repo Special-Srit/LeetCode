0bba4d1dcfd08c09fa7eb41bc4a8b6e1aa654a4b
srit <coder.srit@gmail.com>
2026-06-02 22:53:44 +0900

    Solved `135. Candy`

---
+++ src/Array & String/15. Candy/main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class ClassName {
+    static class Solution {
+        public int candy(int[] ratings) {
+            int n = ratings.length;
+            int[] candies = new int[n];
+            Arrays.fill(candies, 1);
+
+            for (int i = 1; i < n; i++) {
+                if (ratings[i] > ratings[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
+                }
+            }
+
+            for (int i = n - 2; i >= 0; i--) {
+                if (ratings[i] > ratings[i + 1]) {
+                    candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+                }
+            }
+
+            int totalCandies = 0;
+            for (int candy : candies) {
+                totalCandies += candy;
+            }
+            return totalCandies;
+        }
+    }        
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.candy(new int[]{1, 0, 2}));
+        System.out.println(sol.candy(new int[]{1, 2, 2}));
+    }
+}
