67f419e6ecb9cfbe1f29397ffd9b7e196ca9e005
srit <coder.srit@gmail.com>
2026-06-14 23:26:50 +0900

    Solved `167. Two Sum II - Input Array Is Sorted`

---
+++ src/Two Pointers/27. Two Sum II - Input Array Is Sorted/main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class twoSumII {
+    static class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+            while (left < right) {
+                int sum = numbers[left] + numbers[right];
+                if (sum == target) {
+                    return new int[]{left + 1, right + 1};
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            return new int[]{-1, -1};
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9; 
+        int[] result = sol.twoSum(numbers, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
